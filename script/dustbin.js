cd72cab3360f694d94ae9cfd72cf3504887f4a30
SadmanG <sadmangergees@gmail.com>
2026-02-26 01:27:58 +0600

    All delete buttons and Q/As answered

--- script/dustbin.js
+++ script/dustbin.js
@@ -2,3 +2,38 @@ document.getElementById("MFC-db").addEventListener("click", function(){
     const mfc = document.getElementById("MFC");
     mfc.classList.add("hidden");
 })
+
+document.getElementById("WFA-db").addEventListener("click", function(){
+    const wfa = document.getElementById("WFA");
+    wfa.classList.add("hidden");
+})
+
+document.getElementById("DVS-db").addEventListener("click", function(){
+    const dvs = document.getElementById("DVS");
+    dvs.classList.add("hidden");
+})
+
+document.getElementById("CFI-db").addEventListener("click", function(){
+    const cfi = document.getElementById("CFI");
+    cfi.classList.add("hidden");
+})
+
+document.getElementById("IL-db").addEventListener("click", function(){
+    const il = document.getElementById("IL");
+    il.classList.add("hidden");
+})
+
+document.getElementById("MCS-db").addEventListener("click", function(){
+    const mcs = document.getElementById("MCS");
+    mcs.classList.add("hidden");
+})
+
+document.getElementById("SXYZ-db").addEventListener("click", function(){
+    const sxyz = document.getElementById("SXYZ");
+    sxyz.classList.add("hidden");
+})
+
+document.getElementById("TCI-db").addEventListener("click", function(){
+    const tci = document.getElementById("TCI");
+    tci.classList.add("hidden");
+})
